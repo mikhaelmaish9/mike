62263148f60153d5e89414e3bf3b261d5ff016e1
mikhaelmaish9 <mikhaelmaish9@users.noreply.github.com>
2018-04-07 17:53:45 +0300

    saturday7 added

---
+++ saturday7.py
@@ -0,0 +1,32 @@
+#the average age of sarah,mark and fatima
+sarah = 23
+mark = 19
+fatima = 31
+sum = sarah + mark + fatima
+print(sum)
+average = sum / 3
+print(average)
+
+#the number of times 73 goes to 403
+num1 = 403
+num2  = 73
+times = num1 / num2
+print(times)
+#the remainder when 403 is divided by 73
+num1 = 403
+num2 = 73
+rem = num1 % num2
+print(rem)
+
+# 2 to the 10th power
+num1 = 10
+num2 = 2
+power = num1 ** num2
+print("The power is ", power)
+
+#absolute value of difference between sarah's height(54 inches ) and mark's height (57 inches)
+sarah = 54
+mark =57
+abs = sarah - mark
+print(abs)
+
