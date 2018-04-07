8a8d0bfe02e7b63403dfa4f2768362b7c8202573
mikhaelmaish9 <mikhaelmaish9@users.noreply.github.com>
2018-04-07 18:09:33 +0300

    copy of task1

---
+++ task1 - Copy.py
@@ -0,0 +1,13 @@
+# store some weigh in a variable
+# store height in a variable
+# w/h
+# print the answer
+
+weight = 67
+height = 8
+div = weight/height
+print(div)
+
+
+# variable height,weight,div are indentifiers/name
+# indentifiers can be any name
